93cfad5c31a76bf8cee417f43dc7c60e26566cfd
Dominik Kapusta <dkapusta@duckduckgo.com>
2023-01-20 14:32:57 +0100

    Don't import allPRs into macosBrowserPRs

--- org/macosBrowserPRs.ts
+++ org/macosBrowserPRs.ts
@@ -1,5 +1,20 @@
-import {fail, danger} from "danger"
-import allPRs from "./allPRs"
+import {fail, warn, danger} from "danger"
+
+export const prSize = async () => {
+    // Warn when there is a big PR
+    if (danger.github.pr.additions + danger.github.pr.deletions > 500) {
+        warn("PR has more than 500 lines of code changing. Consider splitting into smaller PRs if possible.");
+    }
+}
+
+export const internalLink = async () => {
+    // Warn when link to internal task is missing
+    for (let bodyLine of danger.github.pr.body.toLowerCase().split(/\n/)) {
+        if (bodyLine.includes("task/issue url:") && (!bodyLine.includes("app.asana.com"))) {
+            warn("Please, don't forget to add a link to the internal task");
+        }
+    }
+}
 
 export const xcodeprojConfiguration = async () => {
     const projectFile = "DuckDuckGo.xcodeproj/project.pbxproj";
@@ -13,6 +28,7 @@ export const xcodeprojConfiguration = async () => {
 }
 
 export default async () => {
-    await allPRs()
+    await prSize()
+    await internalLink()
     await xcodeprojConfiguration()
 }
