c5c746abcec8be6573d5d5d38776bdafeafa0a28
Pete <petes44@icloud.com>
2025-02-19 14:27:36 +0000

    update tests and add test

--- tests/prSize.allPRs.test.ts
+++ tests/prSize.allPRs.test.ts
@@ -10,26 +10,42 @@ beforeEach(() => {
     dm.danger = {
         github: {
             pr: {
-                additions: 200,
-                deletions: 10  
+                additions: 200
             }
         },
+        git: {
+            modified_files: ['src/some/file.ts']
+        }
     }
 })
 
 describe("PR diff size checks", () => {
-    it("does not warn with less than 500 lines", async () => {
+    it("does not warn with less than 500 added lines", async () => {
         await prSize()
         
         expect(dm.warn).not.toHaveBeenCalled()
     })
 
-    it("warns with more than 500 lines", async () => {
-        dm.danger.github.pr.deletions = 301
+    it("warns with 500 or more added lines in non-excluded files", async () => {
+        dm.danger.github.pr.additions = 500
+        dm.danger.git.modified_files = ['src/some/file.ts']
 
         await prSize()
         
-        expect(dm.warn).toHaveBeenCalledWith('PR has more than 500 lines of code changing. Consider splitting into smaller PRs if possible.')
+        expect(dm.warn).toHaveBeenCalledWith('PR has 500 or more lines of added code (excluding Xcode projects and assets). Consider splitting into smaller PRs if possible.')
+    })
+
+    it("does not warn when all modified files are excluded", async () => {
+        dm.danger.github.pr.additions = 1000
+        dm.danger.git.modified_files = [
+            'Project.xcodeproj/something',
+            'Assets.xcassets/image.png',
+            'Project.xcworkspace/contents'
+        ]
+
+        await prSize()
+        
+        expect(dm.warn).not.toHaveBeenCalled()
     })
 })
 
